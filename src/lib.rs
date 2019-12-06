5c18a5bc073c7eccbc1704ccda3d70e7bf4cf485
cezhang52111 <zhangce52111@ruc.edu.cn>
2019-12-06 13:52:39 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-#![no_std]
-#![feature(lang_items)]
-#![feature(core_intrinsics)]
-#![feature(global_allocator)]
-#![feature(alloc)]
-
 
 extern crate wasm_std as ext;
 use serde::{Serialize, Deserialize};
@@ -20,17 +14,3 @@ pub fn test_return_args() {
     let args = ext::args();
     ext::return_data(&args);
 }
-
-#[no_mangle]
-pub fn test_set_storage() {
-    let args = ext::args();
-    ext::set_storage(&args, b"you found me!");
-}
-
-#[no_mangle]
-pub fn test_get_storage() {
-    let args = ext::args();
-    let value = ext::get_storage(&args);
-    ext::return_data(&value);
-}
-
